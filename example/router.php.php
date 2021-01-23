5a5698a4d350ee010408a894a708424ad12de11b
Nolan METAYER <nolanmetayer6@gmail.com>
2021-01-23 15:41:13 +0100

    Create router.php.php

---
+++ example/router.php.php
@@ -0,0 +1,27 @@
+<?php
+require 'vendor/autoload.php';
+$router = new src\Router\AltoRouter();
+
+//* EXEMPLE
+
+//router index
+$router->map('GET', '/', 'index', 'index');
+
+// router order 
+$router->map('GET','/order/[i:id]', 'order', 'order');
+
+//router error
+$router->map('GET', '/404', '404', '404');
+
+$match = $router->match();
+
+if(is_array($match)) {
+    if (is_callable($match['target'])) {
+        call_user_func_array($match['target'], $match['params']);
+    } else {
+        $params = $match['params'];
+        require "public/{$match['target']}.php";
+    }
+} else {
+    require "public/404.php";
+}
